8e3567cdc2d78da02449821553612af781e8fd68
Joel Giovinazzo <joelgiovinazzo@gmail.com>
2024-03-19 21:53:42 +1100

    Core logic - guessing word (static word for now), evaluating guesses (no win or lose state yet)

--- go.mod
+++ go.mod
@@ -2,10 +2,15 @@ module termdle
 
 go 1.22.1
 
-require github.com/charmbracelet/bubbletea v0.25.0
+require (
+	github.com/charmbracelet/bubbles v0.18.0
+	github.com/charmbracelet/bubbletea v0.25.0
+)
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v0.9.1 // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
--- main.go
+++ main.go
@@ -5,35 +5,111 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
+	"strings"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+type Guess struct {
+	correct               bool
+	guess                 string
+	correctLettersIndex   []int
+	incorrectLettersIndex []int
+}
+
 type Model struct {
-	word    string
-	guesses []string
+	word     string
+	msg      string
+	maxTries int
+	guesses  []Guess
+
+	guessInput textinput.Model
+}
+
+func checkGuess(word, guess string) Guess {
+	var g Guess
+	g.guess = guess
+
+	// Handle correct guess first
+	if word == guess {
+		g.correct = true
+		g.correctLettersIndex = []int{0, 1, 2, 3, 4}
+		return g
+	}
+
+	// Handle incorrect guess
+	for i, l := range guess {
+		if l == rune(word[i]) {
+			g.correctLettersIndex = append(g.correctLettersIndex, i)
+		} else if slices.Contains(strings.Split(word, ""), string(l)) {
+			g.incorrectLettersIndex = append(g.incorrectLettersIndex, i)
+		}
+	}
+
+	return g
 }
 
 func (m Model) Init() tea.Cmd { return nil }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c":
 			return m, tea.Quit
+		case "enter":
+			if len(m.guesses) < m.maxTries && len(m.guessInput.Value()) == 5 {
+				m.guesses = append(m.guesses, checkGuess(m.word, m.guessInput.Value()))
+				m.guessInput.SetValue("")
+			}
+			return m, nil
 		}
 	}
 
-	return m, nil
+	m.guessInput, cmd = m.guessInput.Update(msg)
+	return m, cmd
 }
 
-func (m Model) View() string { return "Hello, World" }
+func (m Model) View() string {
+	s := "Make a guess!\n"
+	s += fmt.Sprintf("Legend: (correct) [wrong position]\n\n")
+
+	for _, g := range m.guesses {
+		for i, l := range g.guess {
+			if slices.Contains(g.correctLettersIndex, i) {
+				s += fmt.Sprintf("(%s)", string(l))
+			} else if slices.Contains(g.incorrectLettersIndex, i) {
+				s += fmt.Sprintf("[%s]", string(l))
+			} else {
+				s += fmt.Sprintf("%s", string(l))
+			}
+		}
+		s += "\n"
+	}
+	s += fmt.Sprintf("\n%s", m.guessInput.View())
+	s += "\n\nPress Ctrl+C to quit\n"
+
+	return s
+}
 
 func main() {
 	words := []string{"about", "penne"}
+
+	textInput := textinput.New()
+	textInput.Focus()
+	textInput.Placeholder = "Guess a word"
+	textInput.CharLimit = 5
+	textInput.Width = 20
+
 	model := Model{
-		word: words[0],
+		// Choose first word for now
+		word:       words[0],
+		maxTries:   6,
+		guessInput: textInput,
 	}
 
 	p := tea.NewProgram(model)
